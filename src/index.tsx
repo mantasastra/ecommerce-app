3e8d9924c2ff6f705cf1aeefbc18d5900c47ea1e
Mantas Astrauskas <mastrauskas4@gmail.com>
2020-07-27 07:13:22 +0100

    Add and use redux-persist lib to add current cart into local storage

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import store from "./store/store";
+import { store, persistor } from "./store/store";
 import App from "./App";
 
 import "./index.css";
@@ -12,7 +13,9 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
